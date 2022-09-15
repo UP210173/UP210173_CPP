f3999b9dc3a1235ca7fa7a5d3754c090be1492c0
Cristian Morales Perez <up210173@alumnos.upa.edu.mx>
2022-09-15 13:22:41 -0500

    Datos

--- U1/TiposDatos.cpp
+++ U1/TiposDatos.cpp
@@ -11,13 +11,17 @@ using namespace std; //Para los nombre setsandar que maneja el cin y cout
 
 int main() //Regresa un valor entero
 {
-    int entero = 37475421;
+    int entero = 2147483647;
     float flotante = 3.4e38;
     double grande = 7.842765257;
     char caracter = 'C';
 
     cout<<"Este programa muestra los tipos de datos \n";
-    cout<<"El numero entero es: "<<entero<<endl;
-    cout<<"El tamaño del entero es "<<sizeof(entero)<< "bytes" <<endl;
-    cout<<"El número flotante es: "<<flotante<<endl;
+    cout<<"El numero entero es: "<< entero<< endl;
+    cout<<"El tamaño del entero es "<< sizeof(entero) << "bytes" <<endl;
+    cout<<"El caracter char es: " <<endl;
+    cout<<"El tamaño del char es "<< sizeof(caracter) << "bytes" <<endl;
+
+   get char(); //Para correr el programa de windows
+   return 0;
 }
